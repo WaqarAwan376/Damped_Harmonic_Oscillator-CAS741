3e897dbf303e83f59fe2b17c799670ecd1329ead
WaqarAwan376 <tr.awan.waqar@gmail.com>
2024-03-23 19:59:06 -0400

    Refactor: Removed unused code from Number Input component

--- src/dhm-frontend/src/components/UI/NumberInputBasic.jsx
+++ src/dhm-frontend/src/components/UI/NumberInputBasic.jsx
@@ -9,31 +9,17 @@ export const NumberInputBasic = ({
   calculationValues,
   inputType = "",
 }) => {
-  const [value, setValue] = React.useState(0);
-
-  const onChange = (val) => {
-    console.warn("onChange:", val, typeof val);
-    setValue(val);
-  };
   return (
     <div>
       <InputNumber
         aria-label="Simple number input example"
-        // min={-8}
-        // max={10}
         style={{ color: "black" }}
-        // value={value}
-        // onChange={onChange}
         keyboard={true}
         stringMode={false}
         {...(allowNegativeVal ? {} : { min: 0 })}
         step={step}
-        // defaultValue={defaultValue}
         placeholder="Enter a number"
-        // value={value}
         onChange={(val) => {
-          // setValue(val);
-          // !allowNegativeVal && val >= 0 ? setValue(val) : setValue(val);
           const configValues = {
             ...calculationValues,
           };
